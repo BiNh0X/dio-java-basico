ab16c64860b4f309be185e72d803988595807190
BiNh0X <fabiocastro185@gmail.com>
2022-11-02 01:20:27 -0300

    Meu primeiro projeto Java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas-vindas tuma DIO!");
+        System.out.println("Boas-vindas turma DIO!");
     }
 }
