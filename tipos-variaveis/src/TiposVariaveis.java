50b23ef9cc3a0916c63b68eeea4959e0d9b87f17
BiNh0X <fabiocastro185@gmail.com>
2022-11-03 12:48:12 -0300

    Exemplos de tipos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public static void main(String[] args) {
+        
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+	    int numeroNormal = numeroCurto;
+	    short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        System.out.print(numero);
+        
+        numero = 2;
+        System.out.print(numero);
+
+
+        // E se o valor nunca poderá mudar?
+
+        double pi = 3.15;
+        pi = 5.34;
+        System.out.print(pi);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.print(VALOR_DE_PI);
     }
+
 }
