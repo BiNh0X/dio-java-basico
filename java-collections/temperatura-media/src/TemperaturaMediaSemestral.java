24afda3efc90b8d28470bb8c7985f61a136f4189
BiNh0X <fabiocastro185@gmail.com>
2022-11-13 19:22:24 -0300

    Adicionado: Enunciado do exercício.

--- java-collections/temperatura-media/src/TemperaturaMediaSemestral.java
+++ java-collections/temperatura-media/src/TemperaturaMediaSemestral.java
@@ -5,6 +5,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+/*Faça um programa que receba a temperatura média os 6 primeiros meses do ano e armazene-as em uma lista.
+Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média,
+e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro e etc).
+*/
+
 public class TemperaturaMediaSemestral {
     public static void main(String[] args) throws Exception {
         
